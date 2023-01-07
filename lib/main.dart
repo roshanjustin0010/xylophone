615b413352931cc93e6bca860f1e3b63d7b7da9c
roshanjustin0010 <roshanjustin0010@gmail.com>
2023-01-08 01:09:29 +0530

    changed sound function

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      backgroundColor: Colors.black,
       body: Xylophone(),
     ),
   ));
@@ -17,24 +18,47 @@ class Xylophone extends StatefulWidget {
 }
 
 class _XylophoneState extends State<Xylophone> {
+  void playsound(int no) {
+    AssetsAudioPlayer.newPlayer().open(
+      Audio("assets/note$no.wav"),
+      autoStart: true,
+      showNotification: true,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        GestureDetector(
-          onTap: () {
-            AssetsAudioPlayer.newPlayer().open(
-              Audio("assets/note1.wav"),
-              autoStart: true,
-              showNotification: true,
-            );
-          },
-          child: Container(
-            height: 100,
-            width: 100,
-            color: Colors.red,
+        Expanded(
+          child: GestureDetector(
+            onTap: () {
+              AssetsAudioPlayer.newPlayer().open(
+                Audio("assets/note1.wav"),
+                autoStart: true,
+                showNotification: true,
+              );
+            },
+            child: Container(
+              color: Colors.red,
+            ),
           ),
-        )
+        ),
+        Expanded(
+          child: GestureDetector(
+            onTap: () {
+              playsound(3);
+            },
+            child: Container(
+              color: Colors.yellow,
+            ),
+          ),
+        ),
+        OutlinedButton(
+            onPressed: () {},
+            child: Container(
+              color: Colors.black,
+            ))
       ],
     );
   }
